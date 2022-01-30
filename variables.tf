7d3056e5e036a9aae31014bb7d9c1c77a781e3f1
Patrick Mullaley <shushs@gmail.com>
2022-01-30 09:36:58 -0500

    Added some comment documentation

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
+# Zone ID of the AWS Route53 Zone (here or via command line)
 variable "aws_route53_zone" {
   type        = string
   description = "My R53 Zone ID"
 }
 
+# Optional domain name setting. This will be appended to the end of the `Name` tag.
 variable "domain" {
   type    = string
   default = "noperightout.io"
 }
 
+# How many instances to create. Defaults to 2
 variable "num_instances" {
   default = 2
 }
